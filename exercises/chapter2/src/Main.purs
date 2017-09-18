ec9a40121f1cfff09969721a1dd58f15e189ee05
Jimmy Hu <jimmyhu.co@gmail.com>
2017-09-18 19:33:21 +0800

    add type signature of main

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -1,10 +1,12 @@
 module Main where
 
-import Prelude ((+), (*))
-import Control.Monad.Eff.Console (logShow)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Math (sqrt)
+import Prelude (Unit, (*), (+))
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+main :: ∀ t4. Eff ( console ∷ CONSOLE | t4 ) Unit
 main = logShow (diagonal 3.0 4.0)
